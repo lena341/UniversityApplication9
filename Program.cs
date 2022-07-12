fb4b63dfd4b3de11085ec1ce80dd3d06ea16b445
Eleni K <71977337+Lena341@users.noreply.github.com>
2022-07-12 21:27:27 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,20 +3,18 @@ using Microsoft.Extensions.DependencyInjection;
 using ContosoUniversity.Data;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<SchoolContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("myConnectionString")));
+builder.Services.AddDbContext<SchoolContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("myConnectionString")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 else
@@ -28,10 +26,8 @@ else
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
     var context = services.GetRequiredService<SchoolContext>();
     context.Database.EnsureCreated();
-    // DbInitializer.Initialize(context);
 }
 
 app.UseHttpsRedirection();
